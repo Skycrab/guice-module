d139792605147708532dbd38d5664ac4cf8e219b
skycrab <skycrab68@163.com>
2019-10-13 14:15:53 +0800

    refactor(guice-example) 完善example

--- guice-example/src/main/java/com/google/code/guice/example/GuiceExample.java
+++ guice-example/src/main/java/com/google/code/guice/example/GuiceExample.java
@@ -1,9 +1,9 @@
 package com.google.code.guice.example;
 
-import com.google.inject.Binder;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.inject.Module;
+import com.google.inject.*;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by yihaibo on 2019-09-27.
@@ -13,23 +13,39 @@ import com.google.inject.Module;
  * guice觉得基于xml方式太过隐式，而自动装配又过于显示，guice基于两者之间，通过代码控制，较为克制
  *
  */
+@Slf4j
 public class GuiceExample {
     public static void main( String[] args ) throws Exception {
         Injector injector = Guice.createInjector(new Module() {
             @Override
             public void configure(Binder binder) {
-
+                binder.bind(ProduceService.class).to(KafkaPrduceService.class);
+                binder.bind(String.class).annotatedWith(Names.named("server")).toInstance("localhost:9002");
+                binder.bind(String.class).annotatedWith(Names.named("topic")).toInstance("test");
             }
         });
 
-        A a = injector.getInstance(A.class);
-        a.print();
-
+        ProduceService produce = injector.getInstance(ProduceService.class);
+        produce.produce("hello guice");
     }
 
-    public static class  A {
-        public void print() {
-            System.out.println("AAA");
+    public interface ProduceService {
+        void produce(Object msg);
+    }
+
+    @Singleton
+    public static class KafkaPrduceService implements ProduceService {
+        private String server;
+        private String topic;
+
+        @Inject
+        public KafkaPrduceService(@Named("server") String server, @Named("topic") String topic) {
+            this.server = server;
+            this.topic = topic;
+        }
+        @Override
+        public void produce(Object msg) {
+            log.info("produce {}-{}-{}", server, topic, msg);
         }
     }
 }
